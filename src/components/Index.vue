f6875aad50af55e55bb5ccab49dda090769a0527
dabdevelop <develop@dab.vc>
2018-08-21 21:57:49 +0800

    add data refresh function

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -6,17 +6,13 @@
 			<b-navbar-brand href="#">EOS头号玩家</b-navbar-brand>
 			<b-collapse is-nav id="nav_collapse">
 				<b-navbar-nav>
-					<b-nav-item href="#">游戏规则</b-nav-item>
-					<b-nav-item href="#">路线图</b-nav-item>
-					<b-nav-item href="#">安全说明</b-nav-item>
-					<b-nav-item href="#">免责说明</b-nav-item>
 					<b-nav-item href="https://github.com/dabdevelop/playerone/blob/master/playerone/playerone.cpp">合约开源</b-nav-item>
 				</b-navbar-nav>
 				<!-- Right aligned nav items -->
 				<b-navbar-nav class="ml-auto">
 					<b-nav-form>
-						<b-form-input size="sm" class="mr-sm-2" type="text" placeholder="EOS用户名"/>
-						<b-button size="sm" variant="outline-success" class="my-2 my-sm-0" type="submit">搜索</b-button>
+						<b-form-input size="sm" class="mr-sm-2" type="text" placeholder="EOS用户名" v-model="ivtUrl" id="ivtUrl"/>
+						<b-button size="sm" variant="outline-success" class="my-2 my-sm-0" @click.prevent="copyLink">复制邀请链接</b-button>
 					</b-nav-form>
 					<b-nav-item-dropdown text="语言" right>
 						<b-dropdown-item href="#">ZH</b-dropdown-item>
@@ -60,7 +56,7 @@
 						<el-card :body-style="{ padding: '10px' }" shadow="hover">
 							<div slot="header" class="clearfix">
 								<span>合约信息</span>
-								<el-button @click.prevent="get_game" type="primary" style="float: right;" icon="el-icon-refresh" circle></el-button>
+								<el-button @click.prevent="getGame" type="primary" style="float: right;" icon="el-icon-refresh" circle></el-button>
 							</div>
 							储备金：{{game.reserve}}
 							保证金：{{game.insure}}
@@ -83,7 +79,7 @@
 						<el-card :body-style="{ padding: '10px' }" shadow="hover">
 							<div slot="header" class="clearfix">
 								<span>用户信息</span>
-								<el-button type="primary" @click.prevent="get_user"  style="float: right;" icon="el-icon-refresh" circle></el-button>
+								<el-button type="primary" @click.prevent="getUser"  style="float: right;" icon="el-icon-refresh" circle></el-button>
 							</div>
 							用户名：{{user.name}}
 							EOS余额：{{user.eosBalance}}
@@ -104,27 +100,27 @@
 									<p>请输入EOS数量</p>
 									<el-input-number v-model="buyAmount" @change="handleChange" :min="0" :max="100" :precision="2" :step="10" label="描述文字"></el-input-number>
 									<br><br>
-									<el-button @click.prevent="buy" type="primary" plain>购买CGT</el-button>
-									<el-button @click.prevent="deposit" type="info" disabled=true plain>购买邀请码</el-button>
-									<el-button @click.prevent="invite" type="info" disabled=true plain>邀请朋友</el-button>
+									<el-button @click.prevent="buy" type="primary" >购买CGT</el-button>
+									<el-button @click.prevent="deposit" type="info" disabled>购买邀请码</el-button>
+									<el-button @click.prevent="invite" type="info" disabled>邀请朋友</el-button>
 									<br>
 								</el-tab-pane>
 								<el-tab-pane label="购买邀请码" name="second">
 									<p>请输入EOS数量</p>
 									<el-input-number v-model="depositAmount" @change="handleChange" :min="0" :max="100" :precision="2" :step="10" label="描述文字"></el-input-number>
 									<br><br>
-									<el-button @click.prevent="buy" type="info" disabled=true plain>购买CGT</el-button>
-									<el-button @click.prevent="deposit" type="primary" plain>购买邀请码</el-button>
-									<el-button @click.prevent="invite" type="info" disabled=true plain>邀请朋友</el-button>
+									<el-button @click.prevent="buy" type="info" disabled>购买CGT</el-button>
+									<el-button @click.prevent="deposit" type="primary" >购买邀请码</el-button>
+									<el-button @click.prevent="invite" type="info" disabled>邀请朋友</el-button>
 									<br>
 								</el-tab-pane>
 								<el-tab-pane label="邀请朋友" name="third">
 									<p>请输入EOS账号</p>
 									<el-input v-model="invitation" placeholder="EOS账号"></el-input>
 									<br><br>
-									<el-button @click.prevent="buy" type="info" disabled=true plain>购买CGT</el-button>
-									<el-button @click.prevent="deposit" type="info" disabled=true plain>购买邀请码</el-button>
-									<el-button @click.prevent="invite" type="primary" plain>邀请朋友</el-button>
+									<el-button @click.prevent="buy" type="info" disabled>购买CGT</el-button>
+									<el-button @click.prevent="deposit" type="info" disabled>购买邀请码</el-button>
+									<el-button @click.prevent="invite" type="primary" >邀请朋友</el-button>
 									<br>
 								</el-tab-pane>
 								
@@ -138,27 +134,27 @@
 									<p>请输入CGT数量</p>
 									<el-input-number v-model="sellAmount" @change="handleChange" :min="0" :max="1000000" :step="1000" label="描述文字"></el-input-number>
 									<br><br>
-									<el-button @click.prevent="sell" type="danger" plain>出售CGT</el-button>
-									<el-button @click.prevent="burn" type="info" disabled=true plain>销毁CGT</el-button>
-									<el-button @click.prevent="stake" type="info" disabled=true plain>抵押CGT</el-button>
+									<el-button @click.prevent="sell" type="danger" >出售CGT</el-button>
+									<el-button @click.prevent="burn" type="info" disabled>销毁CGT</el-button>
+									<el-button @click.prevent="stake" type="info" disabled>抵押CGT</el-button>
 									<br>
 								</el-tab-pane>
 								<el-tab-pane label="销毁CGT" name="second">
 									<p>请输入CGT数量</p>
 									<el-input-number v-model="burnAmount" @change="handleChange" :min="0" :max="1000000" :step="1000" label="描述文字"></el-input-number>
 									<br><br>
-									<el-button @click.prevent="sell" type="info" disabled=true plain>出售CGT</el-button>
-									<el-button @click.prevent="burn" type="danger" plain>销毁CGT</el-button>
-									<el-button @click.prevent="stake" type="info" disabled=true plain>抵押CGT</el-button>
+									<el-button @click.prevent="sell" type="info" disabled>出售CGT</el-button>
+									<el-button @click.prevent="burn" type="danger" >销毁CGT</el-button>
+									<el-button @click.prevent="stake" type="info" disabled>抵押CGT</el-button>
 									<br>
 								</el-tab-pane>
 								<el-tab-pane label="抵押CGT" name="third">
 									<p>请输入CGT数量</p>
 									<el-input-number v-model="stakeAmount" @change="handleChange" :min="1000" :max="1000000" :step="1000" label="描述文字"></el-input-number>
 									<br><br>
-									<el-button @click.prevent="sell" type="info" disabled=true plain>出售CGT</el-button>
-									<el-button @click.prevent="burn" type="info" disabled=true plain>销毁CGT</el-button>
-									<el-button @click.prevent="stake" type="danger" plain>抵押CGT</el-button>
+									<el-button @click.prevent="sell" type="info" disabled>出售CGT</el-button>
+									<el-button @click.prevent="burn" type="info" disabled>销毁CGT</el-button>
+									<el-button @click.prevent="stake" type="danger" >抵押CGT</el-button>
 									<br>
 								</el-tab-pane>
 							</el-tabs>
@@ -168,103 +164,6 @@
 			</div>
 		</div>
 
-		<br>
-		<div>
-			<div class="col-12 col-lg-12 col-xl-12">
-				<el-card :body-style="{ padding: '10px' }" shadow="hover">
-					<h2 style="text-align:center;">游戏规则</h2>
-					<br><br>
-					<el-collapse v-model="ruleActive" accordion>
-						<el-collapse-item title="一致性 Consistency" name="1">
-							<div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-							<div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
-						</el-collapse-item>
-						<el-collapse-item title="反馈 Feedback" name="2">
-							<div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-							<div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-						</el-collapse-item>
-						<el-collapse-item title="效率 Efficiency" name="3">
-							<div>简化流程：设计简洁直观的操作流程；</div>
-							<div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-							<div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-						</el-collapse-item>
-						<el-collapse-item title="可控 Controllability" name="4">
-							<div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-							<div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-						</el-collapse-item>
-					</el-collapse>
-				</el-card>
-			</div>
-		</div>
-
-		<br>
-		<div>
-			<div class="col-12 col-lg-12 col-xl-12">
-				<el-card :body-style="{ padding: '10px' }" shadow="hover">
-					<h2 style="text-align:center;">路线图</h2>
-					<br><br>
-					<el-steps :active="1" align-center>
-						<el-step title="内测"></el-step>
-						<el-step title="ENU公测"></el-step>
-						<el-step title="正式上线"></el-step>
-						<el-step title="更多游戏"></el-step>
-					</el-steps>
-				</el-card>
-			</div>
-		</div>
-
-		<br>
-		<div>
-			<div class="col-12 col-lg-12 col-xl-12">
-				<el-card :body-style="{ padding: '10px' }" shadow="hover">
-					<h2 style="text-align:center;">安全说明</h2>
-					<br><br>
-					<el-collapse v-model="secureActive" accordion>
-						<el-collapse-item title="多重签名智能合约" name="1">
-							<div>EOS权限体系通过权重（Weight）和阈值（Threshold）来限制合约账户，可以实现多个人共同管理一个账户资金，假设智能合约账户有5个人的公钥，每个公钥的权重为1，权限的总阈值为3，这就表示必须要这5个人中至少3个人签名，才可以转移资金和修改合约代码。</div>
-						</el-collapse-item>
-						<el-collapse-item title="移交智能合约权限" name="2">
-							<div>这种方式也比较安全，我们可以修改合约权限为eosio.prods账户，直接将智能合约权限移交给官方21个节点来控制，未来如果需要转移资金或修改合约，则需要申请节点仲裁。</div>
-						</el-collapse-item>
-						<el-collapse-item title="设置合约黑洞公钥" name="3">
-							<div>这种方式是最接近区块链定义的，我们可以将智能合约权限修改为黑洞公钥，官方在超级节点竞选时临时用过的这个公钥：EOS1111111111111111111111111111111114T1Anm，它的公钥是0值加检验数据生成的，任何人都不知道它的私钥，未来合约也不可能再被转移资金或修改。</div>
-						</el-collapse-item>
-						<el-collapse-item title="合约信息公开" name="4">
-							<div>合约账号：eosioshadows；</div><br>
-							<div>合约地址：https://eospark.com/...eosioshadows</div><br>
-							<div>开源代码：https://github.com/...eos-shadows</div><br>
-						</el-collapse-item>
-					</el-collapse>
-				</el-card>
-			</div>
-		</div>
-		<br>
-		<div>
-			<div class="col-12 col-lg-12 col-xl-12">
-				<el-card :body-style="{ padding: '10px' }" shadow="hover">
-					<h2 style="text-align:center;">免责声明</h2>
-					<br><br>
-					<el-collapse v-model="secureActive" accordion>
-						<el-collapse-item title="多重签名智能合约" name="1">
-							<div>EOS权限体系通过权重（Weight）和阈值（Threshold）来限制合约账户，可以实现多个人共同管理一个账户资金，假设智能合约账户有5个人的公钥，每个公钥的权重为1，权限的总阈值为3，这就表示必须要这5个人中至少3个人签名，才可以转移资金和修改合约代码。</div>
-						</el-collapse-item>
-						<el-collapse-item title="移交智能合约权限" name="2">
-							<div>这种方式也比较安全，我们可以修改合约权限为eosio.prods账户，直接将智能合约权限移交给官方21个节点来控制，未来如果需要转移资金或修改合约，则需要申请节点仲裁。</div>
-						</el-collapse-item>
-						<el-collapse-item title="设置合约黑洞公钥" name="3">
-							<div>这种方式是最接近区块链定义的，我们可以将智能合约权限修改为黑洞公钥，官方在超级节点竞选时临时用过的这个公钥：EOS1111111111111111111111111111111114T1Anm，它的公钥是0值加检验数据生成的，任何人都不知道它的私钥，未来合约也不可能再被转移资金或修改。</div>
-						</el-collapse-item>
-						<el-collapse-item title="合约信息公开" name="4">
-							<div>合约账号：eosioshadows；</div><br>
-							<div>合约地址：https://eospark.com/...eosioshadows</div><br>
-							<div>开源代码：https://github.com/...eos-shadows</div><br>
-						</el-collapse-item>
-					</el-collapse>
-				</el-card>
-			</div>
-		</div>
-		<br>
-		<br>
   </div>
 </template>
 
@@ -295,6 +194,8 @@ export default {
 			ruleActive: '1',
 			secureActive: '1',
 			userName: '用户',
+			ivtUrl: "http://eosplayer.one/?ref=playeronefee",
+			refer: "playeronefee",
 			game:{
 				gameid: "oneplayerone",
 				reserve: "0.0000 EOS",
@@ -302,7 +203,7 @@ export default {
 				max_supply: "10000000.0000 CGT",
 				supply: "0.0000 CGT",
 				balance: "0.0000 CGT",
-				circulation: "1503361.6000 CGT",
+				circulation: "0.0000 CGT",
 				burn: "0.0000 CGT",
 				staked: "0.0000 CGT",
 				fee: "0.0000 EOS",
@@ -343,6 +244,10 @@ export default {
 						type: 'success'
 					});
 					this.userName = this.account.name;
+					this.ivtUrl = "http://eosplayer.one/?ref=" + this.account.name;
+					if(typeof this.$route.query.ref !== 'undefined' && this.$route.query.ref !== '')
+					this.refer = this.$route.query.ref;
+					this.getUser();
 				}
 			}).catch(error => {
 				this.$message({
@@ -351,6 +256,8 @@ export default {
 				});
 			});
 		})
+
+
 		
 	},
 
@@ -372,6 +279,8 @@ export default {
 							type: 'success'
 						});
 						this.userName = this.account.name;
+						this.ivtUrl = "http://eosplayer.one/?ref=" + this.account.name;
+						this.getUser();
 					}
 				}).catch(error => {
 					this.$message({
@@ -388,16 +297,17 @@ export default {
 						type: 'warning'
 				});
 				this.userName = '用户';
+				this.ivtUrl = "http://eosplayer.one/?ref=playeronefee";
 			});
 		},
-		get_height() {
+		getHeight() {
 			this.eosClient.getInfo((error, result) => {
 				if(!error){
 					this.block_height = result.head_block_num;
 				}
 			})
 		},
-		get_game() {
+		getGame() {
 			this.eosClient.getTableRows({
 				json: "true",
 				code: config.gameContract,
@@ -415,7 +325,7 @@ export default {
 				console.error(e);
 			})
 		},
-		get_user() {
+		getUser() {
 			this.eosClient.getTableRows({
 				json: "true",
 				code: config.gameContract,
@@ -453,6 +363,72 @@ export default {
 				console.log(res);
 			})
 		},
+		getActions() {
+			// hard code， 单个用户的操作记录
+			var account = this.account.name;
+			// 如果是全部人的操作记录, account = config.gameContract
+			// var account = config.gameContract;
+			// 获取EOS
+			this.eosClient.getActions({
+				account_name: account,
+				pos: 1,     // INT sequence number of action for this account, -1 for last . default -1
+				offset: 1  // INT get actions [pos,pos+offset] for positive offset or [pos-offset,pos) for negative offset
+			}).then(res => {
+				this.actions = res.actions.map(x => {
+					return x.action_trace.act;
+				}).filter(y => {
+					console.log(y);
+					// buy record
+					if (y.account == config.tokenContract && y.name == "transfer" && y.data.to == config.gameContract) {
+						return true;
+					}
+					// sell record 
+					if (y.account == config.gameTokenContract && y.name == "transfer" && y.data.to == config.gameContract) {
+						return true;
+					}
+					return false;
+				});
+			}, res => {
+				console.log(res);
+			})
+		},
+		refresh(){
+			this.getGame();
+			this.getUser();
+			this.getActions();
+		},
+		intervalRefresh(){
+			setInterval(() => { 
+            	this.refresh();
+        	}, 500)
+		},
+		copyLink(){
+			var key = document.getElementById("ivtUrl");
+			if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+				var el = key.get(0);
+				var editable = el.contentEditable;
+				var readOnly = el.readOnly;
+				el.contentEditable = true;
+				el.readOnly = false;
+				var range = document.createRange();
+				range.selectNodeContents(el);
+				var sel = window.getSelection();
+				sel.removeAllRanges();
+				sel.addRange(range);
+				el.setSelectionRange(0, 999999);
+				el.contentEditable = editable;
+				el.readOnly = readOnly;
+			} else {
+				key.select();
+			}
+			document.execCommand('copy');
+			key.blur();
+			this.$message({
+				message: '邀请链接：'+ this.ivtUrl +' 拷贝成功，快邀请小伙伴来玩吧！',
+				type: 'success'
+			});
+			// alert('私钥拷贝成功');
+		},
 		buy() {
 			const requiredFields = {
 				accounts: [config.eosNetwork]
@@ -461,7 +437,7 @@ export default {
 			var from = this.account.name;
 			var to = config.gameContract;
 			var amount = parseFloat(this.buyAmount).toFixed(4) + ' ' + config.mainToken;
-			var memo = "买入CGT，EOS头号玩家感谢您的支持！ 合约地址：oneplayerone 网址：http://eosplayer.one";
+			var memo = this.refer;
 			var arg = [from, to, amount, memo]
 			this.scatterEosClient.contract(config.tokenContract, { requiredFields }).then(contract => {
 				contract.transfer(...arg).then(tx => {
@@ -470,6 +446,7 @@ export default {
 						message: '购买成功',
 						offset: 100
 					});
+					this.refresh();
 				}).catch(e => {
 					console.log(e)
 				})
@@ -485,7 +462,7 @@ export default {
 			var from = this.account.name;
 			var to = config.gameContract;
 			var amount = parseFloat(this.sellAmount).toFixed(4) + ' ' + config.gameToken; 
-			var memo = "卖出CGT，EOS头号玩家感谢您的支持！ 合约地址：oneplayerone 网址：http://eosplayer.one";
+			var memo = this.refer;
 			var arg = [from, to, amount, memo]
 			this.scatterEosClient.contract(config.gameTokenContract, { requiredFields }).then(contract => {
 				contract.transfer(...arg).then(tx => {
@@ -494,6 +471,7 @@ export default {
 						message: '出售成功',
 						offset: 100
 					});
+					this.refresh();
 				}).catch(e => {
 					console.log(e)
 				})
@@ -519,6 +497,7 @@ export default {
 						message: '销毁成功',
 						offset: 100
 					});
+					this.refresh();
 				}).catch(e => {
 					console.log(e)
 				})
@@ -548,6 +527,7 @@ export default {
 							message: '购买成功',
 							offset: 100
 						});
+						this.refresh();
 					}).catch(e => {
 						if(typeof e.code === 'undefined'){
 							this.$message({
@@ -605,6 +585,7 @@ export default {
 						message: '邀请成功',
 						offset: 100
 					});
+					this.refresh();
 				}).catch(e => {
 					if(typeof e.code === 'undefined'){
 						this.$message({
@@ -650,6 +631,7 @@ export default {
 						message: '抵押成功',
 						offset: 100
 					});
+					this.refresh();
 				}).catch(e => {
 					console.log(e)
 				})
@@ -674,6 +656,7 @@ export default {
 						message: '已取消抵押',
 						offset: 100
 					});
+					this.refresh();
 				}).catch(e => {
 					console.log(e)
 				})
@@ -698,6 +681,7 @@ export default {
 						message: '领取成功',
 						offset: 100
 					});
+					this.refresh();
 				}).catch(e => {
 					console.log(e)
 				})
@@ -722,6 +706,7 @@ export default {
 						message: '领取成功',
 						offset: 100
 					});
+					this.refresh();
 				}).catch(e => {
 					console.log(e)
 				})
@@ -730,11 +715,14 @@ export default {
 			});
 		},
 		handleClick(tab, event) {
-        	console.log(tab, event);
 		},
 		handleChange(value) {
-        	console.log(value);
       	}
+	},
+
+	mounted(){
+		this.getGame();
+		this.intervalRefresh();
 	},
 
 	props: {
