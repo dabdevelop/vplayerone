1aa60020093ff0aebba34ee7b2e75d377b605183
dabdevelop <develop@dab.vc>
2018-08-22 22:07:14 +0800

    update

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -311,14 +311,10 @@ export default {
 			var clientLoaded = this.config.network.blockchain === 'eos'? 'scatterLoaded':'ironmanLoaded';
 			document.addEventListener(clientLoaded, scatterExtension => {
 				if(this.config.network.blockchain === 'enu'){
-					this.deskClient = '安装Ironman';
-					this.deskClientURL = 'http://enuironman.com';
 					this.scatter = window.ironman;
 					this.scatter.requireVersion(1.1);
 					this.scatterEosClient = this.scatter.enu(this.config.network, EOS, this.config.options, "http");
 				} else if(this.config.network.blockchain === 'eos'){
-					this.deskClient = '安装Scatter';
-					this.deskClientURL = 'https://get-scatter.com';
 					this.scatter = window.scatter;
 					this.scatter.requireVersion(3.0);
 					this.scatterEosClient = this.scatter.eos(this.config.network, EOS, this.config.options, "http");
@@ -344,6 +340,16 @@ export default {
 			})
 		}
 		this.mobile = this.mobileAndTabletcheck();
+		if(this.mobile && !tp.isConnected()){
+			this.deskClient = '安装TokenPocket';
+			this.deskClientURL = 'https://www.mytokenpocket.vip';
+		} else if(!this.mobile && this.config.network.blockchain === 'enu'){
+			this.deskClient = '安装Ironman';
+			this.deskClientURL = 'http://enuironman.com';
+		} else if(!this.mobile && this.config.network.blockchain === 'eos') {
+			this.deskClient = '安装Scatter';
+			this.deskClientURL = 'https://get-scatter.com';
+		}
 	},
 
 	methods: {
@@ -429,13 +435,39 @@ export default {
 			if(!tp.isConnected()){
 				if(this.config.network.blockchain === 'eos' && typeof window.scatter === 'undefined'){
 					this.$message({
-						message: '请使用Chrome浏览器并安装scatter钱包，或者在TokenPocket环境下使用！',
+						message: '请使用Chrome浏览器并安装Scatter钱包，或者移动端在TokenPocket环境下使用！',
 						type: 'warning'
 					});
 					return false;
 				} else if(this.config.network.blockchain === 'enu' && typeof window.ironman === 'undefined'){
 					this.$message({
-						message: '请使用Chrome浏览器并安装ironman钱包，或者在TokenPocket环境下使用',
+						message: '请使用Chrome浏览器并安装Ironman钱包，或者移动端在TokenPocket环境下使用',
+						type: 'warning'
+					});
+					return false;
+				}
+			} else {
+				if(this.account.blockchain_id == 5 && this.config.env !== 'enu'){
+					this.$message({
+						message: '钱包和网络不一致，当前为ENU钱包，请切换到ENU主网或者切换到EOS钱包！',
+						type: 'warning'
+					});
+					return false;
+				} else if(this.account.blockchain_id == 4 && this.config.env !== 'eos'){
+					this.$message({
+						message: '钱包和网络不一致，当前为EOS钱包，请切换到EOS主网或者切换到ENU钱包！',
+						type: 'warning'
+					});
+					return false;
+				} else if(this.config.env === 'dev'){
+					this.$message({
+						message: '当前在测试网环境，请切换到EOS主网或ENU主网！',
+						type: 'warning'
+					});
+					return false;
+				} else if(this.account.blockchain_id != 4 && this.account.blockchain_id != 5){
+					this.$message({
+						message: '当前在' + this.config.networkName + '，请切换到' + this.config.mainToken+ '钱包！',
 						type: 'warning'
 					});
 					return false;
@@ -604,8 +636,28 @@ export default {
 			var check = false;
 			(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
 			return check;
-      	},
+		},
+		errorNotice(e){
+			if(typeof e !== 'object'){
+				e = JSON.parse(e);
+			}
+			if(e == {}){
+				this.$message({
+					type: 'info',
+					message: '请先登陆scatter钱包！'
+				}); 
+			} else if(e.code == 402) {
+				this.$message({
+					type: 'warning',
+					message: '已取消！'
+				}); 
+			} else if(e.code == 500) {
+				this.$message.error('交易失败！');
+			}
+			console.log(e);
+		},
 		leaseCPU() {
+			if(!this.clientCheck()) return;
 			var from = this.account.name;
 			var to = this.config.gameContract;
 			var amount = parseFloat(0.1).toFixed(4);
@@ -654,26 +706,8 @@ export default {
 				});
 			}
 		},
-		errorNotice(e){
-			if(typeof e !== 'object'){
-				e = JSON.parse(e);
-			}
-			if(e == {}){
-				this.$message({
-					type: 'info',
-					message: '请先登陆scatter钱包！'
-				}); 
-			} else if(e.code == 402) {
-				this.$message({
-					type: 'warning',
-					message: '已取消！'
-				}); 
-			} else if(e.code == 500) {
-				this.$message.error('交易失败！');
-			}
-			console.log(e);
-		},
 		buy() {
+			if(!this.clientCheck()) return;
 			var from = this.account.name;
 			var to = this.config.gameContract;
 			var amount = parseFloat(this.buyAmount).toFixed(4);
@@ -700,7 +734,6 @@ export default {
 					}
 				});
 			} else {
-				if(!this.clientCheck()) return;
 				const requiredFields = {
 					accounts: [this.config.network]
 				}
@@ -724,6 +757,7 @@ export default {
 			}
 		},
 		sell() {
+			if(!this.clientCheck()) return;
 			var from = this.account.name;
 			var to = this.config.gameContract;
 			var amount = parseFloat(this.sellAmount).toFixed(4); 
@@ -750,7 +784,6 @@ export default {
 					}
 				});
 			} else {
-				if(!this.clientCheck()) return;
 				const requiredFields = {
 					accounts: [this.config.network]
 				}
@@ -775,6 +808,7 @@ export default {
 		},
 		// 退出操作
 		burn() {
+			if(!this.clientCheck()) return;
 			var from = this.account.name;
 			var to = this.config.gameContract;
 			var amount = parseFloat(this.burnAmount).toFixed(4); 
@@ -801,7 +835,6 @@ export default {
 					}
 				});
 			} else {
-				if(!this.clientCheck()) return;
 				const requiredFields = {
 					accounts: [this.config.network]
 				}
@@ -825,6 +858,7 @@ export default {
 			}
 		},
 		deposit() {
+			if(!this.clientCheck()) return;
 			var message = '购买邀请码花费的' + this.config.mainToken + '是不可退回的，请确认操作！';
 			this.$message({
 				type: 'warning',
@@ -856,7 +890,6 @@ export default {
 					}
 				});
 			} else {
-				if(!this.clientCheck()) return;
 				const requiredFields = {
 					accounts: [this.config.network]
 				}
@@ -880,6 +913,7 @@ export default {
 			}
 		},
 		invite() {
+			if(!this.clientCheck()) return;
 			var from = this.account.name;
 			var to = this.config.gameContract;
 			var amount = parseFloat(0.0001).toFixed(4);
@@ -906,7 +940,6 @@ export default {
 					}
 				});
 			} else {
-				if(!this.clientCheck()) return;
 				const requiredFields = {
 					accounts: [this.config.network]
 				}
@@ -930,6 +963,7 @@ export default {
 			}
 		},
 		stake() {
+			if(!this.clientCheck()) return;
 			var from = this.account.name;
 			var to = this.config.gameContract;
 			var amount = parseFloat(this.stakeAmount).toFixed(4); 
@@ -956,7 +990,6 @@ export default {
 					}
 				});
 			} else {
-				if(!this.clientCheck()) return;
 				const requiredFields = {
 					accounts: [this.config.network]
 				}
@@ -980,6 +1013,7 @@ export default {
 			}
 		},
 		unstake() {
+			if(!this.clientCheck()) return;
 			var from = this.account.name;
 			var to = this.config.gameContract;
 			var amount = "0.0003";
@@ -1006,7 +1040,6 @@ export default {
 					}
 				});
 			} else {
-				if(!this.clientCheck()) return;
 				const requiredFields = {
 					accounts: [this.config.network]
 				}
@@ -1030,6 +1063,7 @@ export default {
 			}
 		},
 		collect_fee() {
+			if(!this.clientCheck()) return;
 			var from = this.account.name;
 			var to = this.config.gameContract;
 			var amount = "0.0001";
@@ -1056,7 +1090,6 @@ export default {
 					}
 				});
 			} else {
-				if(!this.clientCheck()) return;
 				const requiredFields = {
 					accounts: [this.config.network]
 				}
@@ -1080,6 +1113,7 @@ export default {
 			}
 		},
 		collect_reward() {
+			if(!this.clientCheck()) return;
 			var from = this.account.name;
 			var to = this.config.gameContract;
 			var amount = "0.0002";
@@ -1106,7 +1140,6 @@ export default {
 					}
 				});
 			} else {
-				if(!this.clientCheck()) return;
 				const requiredFields = {
 					accounts: [this.config.network]
 				}
